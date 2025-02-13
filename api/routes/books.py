42b343dd97295f6a4bef9b99f02e3559ebd5fcda
iam-ukahconstantine <constantineukah@gmail.com>
2025-02-13 15:06:41 +0100

    Added get book by ID endpoint

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -60,3 +60,14 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+
+# Retrieve a book by its ID - The missing endpoint
+@router.get("/api/v1/books/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int):
+    book = InMemoryDB.BOOKS.get(book_id) 
+
+    if not book:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content=book)
